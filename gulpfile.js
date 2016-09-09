c3b45abae65195183217392d4b49e82cc30bfbb6
skijit <Tom.Skjei@NewMarket.com>
2016-09-09 16:05:07 -0400

    updated header for better flow, responsiveness, margins

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,55 @@ var gulp = require('gulp'),
   livereload = require('gulp-livereload'),
   path = require('path'),
   ts = require('gulp-typescript');
+  
+var targetDir = './bin/target';
 
-gulp.task('transpile', function() {
-  var targetDir = './bin/target';
+gulp.task('transpile', function() {  
   var tsProject = ts.createProject("tsconfig.json");
   tsProject .src()
             .pipe(ts(tsProject))
-            //.js.pipe(gulp.dest("."));
             .pipe(gulp.dest(targetDir));
-  gulp.src('./package.json').pipe(gulp.dest(targetDir));
-  gulp.src('./public/**/*').pipe(gulp.dest(targetDir+'/public'));
-  gulp.src('./views/**/*').pipe(gulp.dest(targetDir+'/views'));
-            
 });
+
+gulp.task('copyStaticFiles', function() {
+  gulp.src('./public/**/*.{css,ico,js}').pipe(gulp.dest(targetDir+'/public'));  
+});
+
+gulp.task('copyPackageJson', function() {
+  gulp.src('./package.json').pipe(gulp.dest(targetDir));
+});
+
+gulp.task('copyViews', function() {
+  gulp.src('./views/**/*').pipe(gulp.dest(targetDir+'/views'));
+})
 
 gulp.task('develop', function () {
   livereload.listen();
   nodemon({
     script: 'bin/www',
-    ext: 'handlebars ts',
+    ext: 'handlebars ts css',
     ignore: ["node_modules/**/*", "bin/target/**/*"],
     tasks: function(changedFiles) {
+      var execTasks = [ 'copyPackageJson' ];
+      var bTranspile = false, bCopyStaticFiles = false, bCopyViews = false;
+       
       //only invoke transpile task when ts files have changed
-      var bTranspile = false;
       for (var i = 0; i < changedFiles.length; i++) {
-        if (path.extname(changedFiles[i]) === '.ts') {
+        if (path.extname(changedFiles[i]) === '.ts' && !bTranspile) {
           bTranspile = true;
           console.log('Gulp -> Need to Transpile TS files due to change in ' + changedFiles[i]);
-          break;
+          execTasks.push('transpile');
+        } else if (path.extname(changedFiles[i]) === '.css' && !bCopyStaticFiles) {
+          bCopyStaticFiles = true;
+          console.log('Gulp -> Need to copy css files due to change in ' + changedFiles[i]);
+          execTasks.push('copyStaticFiles');
+        } else if (path.extname(changedFiles[i]) === '.handlebars' && !bCopyViews) {
+          bCopyViews = true;
+          console.log('Gulp -> Need to copy views due to change in ' + changedFiles[i]);
+          execTasks.push('copyViews');
         }
       }
-      return bTranspile ? ['transpile'] : [];
+      return execTasks;
     },
     stdout: false
   }).on('readable', function () {
